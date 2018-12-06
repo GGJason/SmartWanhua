988e5ea3efb09636ae0f741688de1fcbd0613849
GGJason <ggjason.tw@gmail.com>
2018-12-07 01:18:57 +0800

    reset gitignore

--- Pods/Target Support Files/Pods-innovation competitionTests/Pods-innovation competitionTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_innovation_competitionTests : NSObject
-@end
-@implementation PodsDummy_Pods_innovation_competitionTests
-@end
--- Pods/Target Support Files/Pods-innovation competitionUITests/Pods-innovation competitionUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_innovation_competitionUITests : NSObject
-@end
-@implementation PodsDummy_Pods_innovation_competitionUITests
-@end
